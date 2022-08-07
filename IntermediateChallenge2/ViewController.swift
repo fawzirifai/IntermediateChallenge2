7683946613b5608acddb62acbc1f702f4b67f0c9
penguininfrefai <f.refai@penguinin.com>
2022-08-07 07:34:17 +0300

    Initial Commit

---
+++ IntermediateChallenge2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntermediateChallenge2
+//
+//  Created by Fawzi Rifai on 07/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
