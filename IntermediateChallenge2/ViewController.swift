a9152d2b7490336ab935e93b44d902f229b617b8
penguininfrefai <f.refai@penguinin.com>
2022-08-08 22:11:43 +0300

    Ability to choose photos from the camera or the photo library and show them on the screen.

--- IntermediateChallenge2/ViewController.swift
+++ IntermediateChallenge2/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var choosePhotoButton: UIButton!
+    @IBOutlet var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func selectPhoto() {
+        let alertController = UIAlertController(title: "Choose from...", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Camera", style: .default) { _ in
+            self.choosePhoto(from: .camera)
+        })
+        alertController.addAction(UIAlertAction(title: "Photo Library", style: .default) { _ in
+            self.choosePhoto(from: .photoLibrary)
+        })
+        alertController.addAction(UIAlertAction.init(title: "Cancel", style: .cancel))
+        self.present(alertController, animated: true)
+        
+    }
+    
+    func choosePhoto(from sourceType: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.sourceType = sourceType
+            imagePickerController.delegate = self
+            present(imagePickerController, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        guard let image = info[.originalImage] as? UIImage else { return }
+        imageView.image = image
+        imageView.isHidden = false
+        choosePhotoButton.isHidden = true
+    }
 
 }
 
